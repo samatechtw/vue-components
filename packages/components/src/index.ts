25ac2f342f73b3ddf719badbaaf38aae40f1d14c
Sam Pullman <sampullman@gmail.com>
2024-10-27 19:47:49 +0800

    feat: export component utils

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 export * from '@samatech/st-multiselect'
 export * from '@samatech/st-input'
+export * from '@samatech/component-utils'
 
 import '@samatech/st-multiselect/dist/style.css'
 import '@samatech/st-input/dist/style.css'
